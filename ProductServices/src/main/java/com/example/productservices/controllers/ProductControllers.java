ee2fc479ea1db4c1be1601d43967279d065ba279
mehdifassifihri <m.fassifihri12@gmail.com>
2023-12-23 11:36:49 +0100

    first commit

--- ProductServices/src/main/java/com/example/productservices/controllers/ProductControllers.java
+++ ProductServices/src/main/java/com/example/productservices/controllers/ProductControllers.java
@@ -1,5 +1,6 @@
 package com.example.productservices.controllers;
 
+import com.example.productservices.dto.CategoryDTO;
 import com.example.productservices.entities.Product;
 import com.example.productservices.external.CategoryService;
 import com.example.productservices.models.ProductResponse;
@@ -7,6 +8,7 @@ import com.example.productservices.services.ProductServicesImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.List;
 
@@ -17,6 +19,9 @@ public class ProductControllers {
     ProductServicesImpl productServices;
     @Autowired
     CategoryService categoryService;
+
+    @Autowired
+    RestTemplate restTemplate;
     @GetMapping("/rest")
     ResponseEntity<List<ProductResponse>> getProducts(){
         return productServices.getProducts();
@@ -32,4 +37,10 @@ public class ProductControllers {
         return productServices.addProduct(product);
     }
 
+
+    @GetMapping("/hello")
+    public String getData() {
+        String url = "http://localhost:5001/api/Mehdi"; // Replace with your target URL
+        return restTemplate.getForObject(url, String.class);
+    }
 }
--- ProductServices/src/main/java/com/example/productservices/services/ProductServices.java
+++ ProductServices/src/main/java/com/example/productservices/services/ProductServices.java
@@ -9,6 +9,7 @@ import java.util.List;
 public interface ProductServices {
     ResponseEntity<List<ProductResponse>> getProducts();
 
+
     ResponseEntity<List<ProductResponse>> getProductsWithFeign();
     Product addProduct(Product product);
 }
